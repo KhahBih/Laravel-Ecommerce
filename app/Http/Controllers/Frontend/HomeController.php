abd1ae1ed2c6e73cfe97748a69ba4a4df29399e8
KhahBih <www.kbinh@gmail.com>
2024-11-02 23:27:34 +0700

    2-11

--- app/Http/Controllers/Frontend/HomeController.php
+++ app/Http/Controllers/Frontend/HomeController.php
@@ -5,11 +5,13 @@ namespace App\Http\Controllers\Frontend;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Slider;
+use App\Models\FlashSale;
 
 class HomeController extends Controller
 {
     public function index(){
         $sliders = Slider::where('status', 1)->orderBy('serial', 'asc')->get();
-        return view('frontend.home.home', compact('sliders'));
+        $flashSaleDate = FlashSale::first();
+        return view('frontend.home.home', compact('sliders', 'flashSaleDate'));
     }
 }
